2537f6510db5b3475dd058734231657b89988418
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-14 09:18:59 -0300

    Finished styling for Countdown component

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import styles from './styles.module.sass'
 
+
 export const Countdown = () => {
+    const [time, setTime] = useState<number>(0)
+
+    const hours = Math.floor(time / 3600)
+    const minutes = Math.floor(time / 60 - hours * 60)
+    const seconds = Math.floor(time % 60)
+
+    const [hoursLeft, hoursRight] = String(hours).padStart(2, '0').split('')
+    const [minutesLeft, minutesRight] = String(minutes).padStart(2, '0').split('')
+    const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('')
+
     return (
         <div className={styles.container}>
-            
+            <div>
+                <span>{hoursLeft}</span>
+                <span>{hoursRight}</span>
+            </div>
+                <span>:</span>
+            <div>
+                <span>{minutesLeft}</span>
+                <span>{minutesRight}</span>
+            </div>
+                <span>:</span>
+            <div>
+                <span>{secondsLeft}</span>
+                <span>{secondsRight}</span>
+            </div>
         </div>
     )
 }
