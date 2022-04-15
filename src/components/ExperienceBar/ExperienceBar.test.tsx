b06981a5b0b74582cabe64a7fa8099e59a089396
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2022-04-15 19:46:11 -0300

    Added tests for ExperienceBar component

---
+++ src/components/ExperienceBar/ExperienceBar.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react"
+import { ExperienceBar } from "."
+import { LevelContext } from "../../context/LevelContext"
+import renderer from 'react-test-renderer'
+
+
+describe("ExperienceBar component", () => {
+    it("should render correctly", () => {
+        render (
+            <LevelContext.Provider value={{ currentExperience: 300, experienceToNextLevel: 1440  } as any }>
+                <ExperienceBar />
+            </LevelContext.Provider>
+        )
+
+        const initialExperience = screen.queryAllByText(/0 xp/)[0]
+        const currentExperience = screen.getByText(/300 xp/)
+        const experienceToNextLevel = screen.getByText(/1440 xp/)
+
+        expect(initialExperience).toBeInTheDocument()
+        expect(currentExperience).toBeInTheDocument()
+        expect(experienceToNextLevel).toBeInTheDocument()
+    })
+
+    it('should match the snapshot', () => {
+        const snapshot = renderer
+            .create(
+                <LevelContext.Provider value={{ currentExperience: 350, experienceToNextLevel: 10440 } as any}>
+                    <ExperienceBar />
+                </LevelContext.Provider>
+            ).toJSON()
+
+            expect(snapshot).toMatchSnapshot()
+      });
+})
